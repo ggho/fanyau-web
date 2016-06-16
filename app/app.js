f010d1e1f8e40ccf61fe3fba4b985d20aec2d113
Gigi Ho <gigiho.dev@gmail.com>
2016-06-16 17:49:15 +0800

    first working ng version

---
+++ app/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$locationProvider', '$routeProvider', '$httpProvider', function($locationProvider, $routeProvider, $httpProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/view1'});
+
+  // $httpProvider.defaults.useXDomain = true;
+  // delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);
---
+++ app/view1/view1.js
@@ -0,0 +1,80 @@
+'use strict';
+
+angular.module('myApp.view1', [
+	'ngRoute'
+	// 'myApp.iframe.onload-directive'
+	])
+
+.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/view1', {
+		templateUrl: 'view1/view1.html',
+		controller: 'View1Ctrl'
+	});
+}])
+
+.controller('View1Ctrl', ['$scope', '$location', '$http', function($scope, $location, $http) {
+
+	$scope.formData = {};
+
+
+
+	//iframe onload
+	 $scope.iframeLoadedCallback = function(temp){
+	 	console.log(temp);
+	 	console.log(this);
+	 	
+    	$location.path('/view2');
+    	$scope.$apply();
+
+    }
+
+
+	// process the form
+	$scope.processForm = function() {
+		$http({
+			method  : 'POST',
+			url     : 'https://docs.google.com/forms/d/1zjikVFl9ZGzG4WroEYloA9KSzvyiZP_l2wk19BdL8LA/formResponse',
+			data    : {
+				
+				'entry.2052552941':'Female',
+				'entry.1577935524':'<= 20'
+			},  // pass in data as strings
+			headers : { 
+			'Content-Type': 'application/x-www-form-urlencoded'
+			}  // set the headers so angular passing info as form data (not request payload)
+
+
+		})
+		.success(function(data) {
+			console.log(data);
+
+			// if (!data.success) {
+			// 	// if not successful, bind errors to error variables
+			// 	$scope.errorName = data.errors.name;
+			// 	$scope.errorSuperhero = data.errors.superheroAlias;
+			// } else {
+   //    			// if successful, bind success message to message
+   //    			$scope.message = data.message;
+   //    		}
+      	});
+	};
+
+	// $scope.processForm();
+
+}])
+
+.directive('iframeOnload', [function(){
+return {
+    scope: {
+        callback: '&iframeOnload'
+    },
+    link: function(scope, element, attrs){
+        
+        element.on('load', function(){
+           return scope.callback({temp: 'test'});
+         
+        })
+    }
+}}]);
+
+
